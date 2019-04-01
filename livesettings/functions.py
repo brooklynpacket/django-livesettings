7f6c6205686a421dd911f18a3e0d48f4a437cfe8
Giovanni Go <giovanni@jamcity.com>
2019-04-01 14:11:58 -0700

    Fix for py3 compatibility

--- livesettings/functions.py
+++ livesettings/functions.py
@@ -27,7 +27,7 @@ class ConfigurationSettings(object):
             try:
                 return self[key]
             except:
-                raise AttributeError, key
+                raise AttributeError(key)
 
         def __iter__(self):
             for v in self.groups():
