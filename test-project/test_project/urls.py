d3bd49331a5227bd8e9daf1f389a79c246e00e47
Giovanni Go <giovanni@jamcity.com>
2022-02-24 14:50:21 -0800

    Fix urls in test_project

--- test-project/test_project/urls.py
+++ test-project/test_project/urls.py
@@ -1,11 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.urls import include, path
 from django.contrib import admin
-admin.autodiscover()
 
-urlpatterns = patterns('',
-    (r'^settings/', include('livesettings.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'^accounts/login/', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
-    (r'^$', 'localsite.views.index')
-
-)
+urlpatterns = [
+    path('settings/', include('livesettings.urls')),
+    path('admin/', include(admin.site.urls)),
+    path('accounts/login/', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
+    path('', 'localsite.views.index')
+]
